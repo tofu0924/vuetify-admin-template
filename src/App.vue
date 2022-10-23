30ef3330d4f69e70c8aae00bbae9aaa1db2bedc9
tofu0924 <in_no@naver.com>
2022-10-23 20:18:04 +0900

    aaa

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,87 @@
 <template>
   <v-app>
-    <v-system-bar color="primary" dark height="50px" align="center" window>
+    <v-system-bar
+      color="primary"
+      dark
+      height="50px"
+      align="center"
+      app
+    >
       <div>3D Rendering</div>
-      <input v-model="layerId" class="width: 100px" />
+      <input
+        v-model="windowHeight"
+        class="width: 100px"
+      >
       <v-spacer />
     </v-system-bar>
 
-    <splitpanes vertical>
-      <pane min-size="5" :size="size" max-size="50">
-        <v-navigation-drawer
-          :width="`{size}%`"
-          color="secondary"
-          dark
-          permanent
+    <v-navigation-drawer
+      :width="width"
+      color="secondary"
+      dark
+      permanent
+      app
+    >
+      <!-- v-slot:img = #img -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            3D Layer
+          </v-list-item-title>
+          <v-list-item-subtitle> visible setting </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider />
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="(item, index) in layerInfos"
+          :key="index"
+          class="pa-0 ma-0"
         >
-          <!-- v-slot:img = #img -->
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="text-h6"> 3D Layer </v-list-item-title>
-              <v-list-item-subtitle> visible setting </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-divider />
-          <v-list dense nav>
-            <v-list-item
-              v-for="(item, index) in layerInfos"
-              :key="index"
-              class="pa-0 ma-0"
-            >
-              <v-checkbox
-                v-model="item.check"
-                :label="`Layer: ${item.layer}`"
-                class="pa-0 ma-0 shrink-mr-2"
-                :color="item.csscolor"
-              />
-            </v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-      </pane>
-
-      <pane>
-        <v-main>
-          <v-container class="ma-0 pa-0" fluid>
-            <v-row>
-              <v-col cols="12">
-                <v-card
-                  class="ma-5 pa-0"
-                  flat
-                  outlined
-                  color="secondary"
-                  height="800px"
-                />
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-main>
-      </pane>
-    </splitpanes>
+          <v-checkbox
+            v-model="item.check"
+            :label="`Layer: ${item.layer}`"
+            class="pa-0 ma-0 shrink-mr-2"
+            :color="item.csscolor"
+          />
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <div
+      class="splitter"
+      :style="`left: ${width -15}px`"
+    />
+    <v-main>
+      <v-container
+        class="ma-0 pa-0"
+        fluid
+      >
+        <v-row>
+          <v-col
+            cols="12"
+          >
+            <v-card
+              class="ma-5 pa-0"
+              flat
+              outlined
+              color="secondary"
+              height="800px"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import { Splitpanes, Pane } from "splitpanes";
-import "splitpanes/dist/splitpanes.css";
-
 export default {
   name: "App",
-  components: { Splitpanes, Pane },
   data: () => ({
-    size: 20,
+    width: 256,
     layerInfos: [
       { layer: 0, color: [0, 0, 0], check: true, csscolor: "#000000" },
       { layer: 1, color: [0, 0, 0], check: true, csscolor: "#fcfcfc" },
@@ -81,12 +94,7 @@ export default {
       { layer: 8, color: [0, 0, 0], check: true, csscolor: "#bbf9df" },
       { layer: 9, color: [0, 0, 0], check: true, csscolor: "#7bc8f6" },
       { layer: 10, color: [0, 0, 0], check: true, csscolor: "#fc5a50" },
-      { layer: 11, color: [0, 0, 0], check: true, csscolor: "#ff81c0" },
-      { layer: 12, color: [0, 0, 0], check: true, csscolor: "#dbb40c" },
-      { layer: 13, color: [0, 0, 0], check: true, csscolor: "#00ffff" },
-      { layer: 14, color: [0, 0, 0], check: true, csscolor: "#7e1e9c" },
-      { layer: 15, color: [0, 0, 0], check: true, csscolor: "#e6daa6" },
-      { layer: 16, color: [0, 0, 0], check: true, csscolor: "#3d1c02" },
+      { layer: 11, color: [0, 0, 0], check: true, csscolor: "#ff81c0" }
     ],
     right: null,
     gradient: "rgba(0,0,0,.7), rgba(0,0,0,.7)",
@@ -102,7 +110,21 @@ export default {
       { title: "Icons", icon: "mdi-emoticon-excited-outline", to: "/icons" },
     ],
     layerId: 1,
+    windowWidth: window.innerWidth,
+    windowHeight: window.innerHeight,
   }),
+  mounted() {
+    window.addEventListener("resize", this.myEventHandler);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.myEventHandler);
+  },
+  methods: {
+    myEventHandler(e) {
+      this.windowHeight = window.innerHeight;
+      this.windowHeight = window.innerWidth;
+    }
+  }
 };
 </script>
 
@@ -111,17 +133,12 @@ export default {
   height: 34px !important;
 }
 
-.splitpanes {
-  background-color: transparent;
+.splitter{
+  position: absolute !important;
+  height: 100px;
+  z-index: 100000;
+  width: 15px !important;
+  background-color: red;
 }
 
-.splitpanes--vertical > .splitpanes__splitter {
-  min-width: 10px;
-  background-color: transparent;
-}
-
-.splitpanes--horizontal > .splitpanes__splitter {
-  min-width: 10px;
-  background-color: transparent;
-}
 </style>
